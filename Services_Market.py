cbd0dc75b9d1126b7f4d7b1295649fefccb5446a
Sebastian Ochoa <s3bas8am@gmail.com>
2026-06-03 09:00:00 -0500

    creación inicial de clases

--- Services_Market.py
+++ Services_Market.py
@@ -1,19 +1,33 @@
 from socketserver import ThreadingTCPServer,BaseRequestHandler
 
 class proveedor:
-	def __init__(self, id, nombre, codigo, costo, ip, puerto):
+	def __init__(self, id, nombre, servicio, costo, ip, puerto):
 		self.id_proveedor = id
 		self.nombre = nombre
-		self.codigo = codigo
+		self.servicio = servicio
 		self.costo = costo
 		self.ip = ip
 		self.puerto = puerto
 
 	def __str__(self):
-		return (
-			f"{self.id_proveedor}. "
-			f"{self.nombre} | "
-			f"{self.codigo} | "
-			f"${self.costo} COP | "
-			f"{self.ip}:{self.puerto}" )
+		return (f"id:{self.id_proveedor}. "
+			f"nombre:{self.nombre} | "
+			f"servicio:{self.servicio} | "
+			f"costo:${self.costo} COP | "
+			f"ip:{self.ip}:{self.puerto}" )
+
+class BaseDatosProveedores:
+	def __init__(self):
+		self.proveedores = []
+		self.siguiente_id = 1
+
+	def registrar(self, nombre, servicio, costo, ip, puerto):
+		proveedor = Proveedor(self.siguiente_id, nombre, servicio, costo, ip, puerto)
+		self.proveedores.append(proveedor)
+		self.siguiente_id +=1
+		return proveedor
+
+	def listar(self):
+		return self.proveedores
+
 
